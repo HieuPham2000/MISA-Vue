65f6a8d6d658e5678e1f0ce71c40b603abc3d1f6
HieuPham2000 <hieu.pt183535@gmail.com>
2021-07-21 14:13:05 +0700

    fix: combobox

--- src/components/base/BaseCombobox.vue
+++ src/components/base/BaseCombobox.vue
@@ -1,29 +1,25 @@
 <template>
-  <div class="m-combobox" :class="{open: isOpen}" :id="id" >
-    <input type="hidden" :fieldName="inputFieldName" v-model="cbxValue">
+  <div class="m-combobox" :class="{open: isOpen}" :id="id" @keydown="moveAway($event)">
+    <input type="hidden" :fieldName="inputFieldName" v-model="value">
     <input
       class="m-combobox__input"
       type="text"
       v-model="text"
       :placeholder="placeholder"
-       @focus="openDropdown"
+      @focus="openDropdown"
+      @input="openDropdown"
+      @keydown="pressKeyInCombobox($event)"
     />
-    <div class="m-combobox__btn" tabindex="-1" @click="toggleDropdown">
+    <div class="m-combobox__btn" tabindex="-1" @click="toggleDropdown" @keydown="pressKeyInCombobox($event)">
       <i class="fas fa-chevron-down"></i>
     </div>
-    <ul class="m-combobox__dropdown" style="overflow: auto">
+    <ul class="m-combobox__dropdown">
       <li 
-        class="m-combobox__dropdown__item" 
-        v-for="(item, index) in defaultItems" 
-        :key="-index"
-        @click="selectDropdownItem(item, -1)"
-      ><i class="fas fa-check"></i>{{ item }}</li>
-      <li 
-        class="m-combobox__dropdown__item" 
-        v-for="item in comboboxItems" 
-        :key="item[dataId]"
+        v-for="(item, index) in showItems" 
+        :class="['m-combobox__dropdown__item', {active: currentItem == index, selected: item[dataName] == text}]" 
+        :key="index"
         @click="selectDropdownItem(item[dataName], item[dataId])"
-      ><i class="fas fa-check"></i>{{ item[dataName] }}</li>
+      ><i class="fas fa-check"></i>{{item[dataName]}}</li>
     </ul>
   </div>
 </template>
@@ -34,6 +30,7 @@
 
 <script>
 import axios from 'axios';
+import {CommonFunction} from '../../script/common/common';
 export default {
   props: {
     id: String,
@@ -41,8 +38,18 @@ export default {
     dataId: String,
     api: String,
     placeholder: String,
-    defaultItems: Array,
-    fixedData: Array,
+    extraItems: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+    fixedData: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
     inputFieldName: String,
     value: {
       type: [Number, String]
@@ -52,45 +59,79 @@ export default {
     return {
       comboboxItems: [],
       isOpen: false,
-      cbxValue: this.value,
+      showItems: [],
       text: null,
+      currentItem: 0,
+      flag: true
     }
   },
   created() {
     let me = this;
     me.loadComboboxData().then(function() {
-      me.setTextByValue();
+      // test
+      me.showItems = me.comboboxItems;
+
+      me.setTextByValue(); 
     });
   },
   watch: {
-    cbxValue: function() {
-      this.$emit("input", this.cbxValue);
+    text: function() {
+      var len = this.text.length;
+      var textNonAccent = CommonFunction.nonAccentVietnamese(this.text);
+      this.showItems = this.comboboxItems.filter((item) => {
+        var itemTextSliceNonAccent = CommonFunction.nonAccentVietnamese(item[this.dataName].slice(0, len));
+        return textNonAccent == itemTextSliceNonAccent;
+      })
     }
   },
+  mounted() {
+    document.addEventListener("click", this.handleClickOutside);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleClickOutside);
+  },
+
   methods: {
+    handleClickOutside(event) {
+      if (!this.$el.contains(event.target)) {
+        this.isOpen = false;
+      }
+    },
+    moveAway($event) {
+      if ($event.keyCode == 9) {
+        this.closeDropdown();
+      }
+    },
     loadComboboxData: function() {
       return axios.get(this.api)
-      .then((res) => { this.comboboxItems =  res.data;})
+      .then((res) => { 
+        this.comboboxItems =  [...this.extraItems, ...res.data];
+      })
       .catch(() => {this.comboboxItems = this.fixedData});
     },
     toggleDropdown: function() {
       this.isOpen = ! this.isOpen;
     },
     closeDropdown: function() {
-      this.isOpen = false;
+        this.isOpen = false;
     },
     openDropdown: function() {
+      this.currentItem = 0;
       this.isOpen = true;
+    },
+    changeValue(value) {
+      this.$emit("input", value);
     },
     selectDropdownItem: function(text, value) {
       this.text = text;
-      this.cbxValue = value;
+      this.changeValue(value);
       this.closeDropdown();
     },
     setTextByValue() {
       var len = this.comboboxItems.length;
       for(var i = 0; i < len; ++i) {
         var item = this.comboboxItems[i];
+        
         if(item[this.dataId] == this.value) {
           this.text = item[this.dataName];
           break;
@@ -107,6 +148,26 @@ export default {
       }
 
       return null;
+    },
+    pressKeyInCombobox($event) {
+      if ( ! this.isOpen && ($event.key == "Enter" || ($event.altKey && $event.keyCode == 40)) ){
+        this.openDropdown();
+      } else if ($event.altKey && $event.keyCode == 38) {
+        this.closeDropdown();
+      } else if ($event.keyCode == 40) {
+        this.currentItem ++;
+        if(this.currentItem >= this.showItems.length) {
+          this.currentItem = 0;
+        }
+      } else if ($event.keyCode == 38) {
+        this.currentItem --;
+        if(this.currentItem < 0) {
+          this.currentItem = this.showItems.length - 1;
+        }
+      } else if ($event.key == "Enter") {
+        var item = this.showItems[this.currentItem];
+        this.selectDropdownItem(item[this.dataName], item[this.dataId]);
+      }
     }
   },
 
