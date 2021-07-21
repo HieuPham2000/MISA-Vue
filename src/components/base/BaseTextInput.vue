c7c96da69778294aa8a433f287d71ef75b0626f0
HieuPham2000 <hieu.pt183535@gmail.com>
2021-07-21 14:20:19 +0700

    add: basic base input

--- src/components/base/BaseTextInput.vue
+++ src/components/base/BaseTextInput.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="input-wrapper">
-    <input type="text" :placeholder="placeholder">
-    <div class="btn-clear-text">
+    <input type="text" :placeholder="placeholder" :maxlength="maxlength">
+    <div class="btn-clear-text" @click="clearText">
         <img src="@/assets/icon/x.svg" >
     </div> 
   </div>
 </template>
 
 <script>
+
 export default {
   props: {
     placeholder: String,
     maxlength: String
+  },
+  methods: {
+
   }
 }
 </script>
