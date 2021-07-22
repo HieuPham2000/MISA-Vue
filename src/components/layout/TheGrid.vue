5deb79c39c54e5cb1ab3b12b7f33fa5a0703a015
HieuPham2000 <hieu.pt183535@gmail.com>
2021-07-22 07:34:20 +0700

    add: popup when delete + comment in TheGrid

--- src/components/layout/TheGrid.vue
+++ src/components/layout/TheGrid.vue
@@ -54,41 +54,66 @@
 
 import axios from 'axios';
 import eventBus from '../../event-bus'
-import {TOAST_TYPE} from '../../type'
+import {TOAST_TYPE, POPUP_TYPE, EMPLOYEE_ACTION} from '../../type'
 import { CommonFunction } from "../../script/common/common";
 export default {
   props: {
-    entityId: String,
-    tableColumns: Array,
-    deleteApi: String,
-    tableDataApi: String
+    entityId: String, // tên trường id, VD: EmployeeId
+    tableColumns: Array, // danh sách các cột trong bảng (và thông tin đi kèm)
+    deleteApi: String, // api dùng khi xóa dữ liệu
+    tableDataApi: String  // api dùng để load dữ liệu
   },
+
   data() {
     return {
-      tableData: [],
-      selectedRows: []
+      tableData: [], // Mảng dữ liệu nhận được sau khi load api
+      selectedRows: [], // Mảng các hàng được click chọn
+      dataPopUpDelete: { // Thông tin về pop up Xóa
+        type: POPUP_TYPE.DANGER,
+        title: "Xóa bản ghi",
+        content: 'Bạn có chắc muốn "Xóa bản ghi" hay không?',
+        btnCancel: "Hủy",
+        btnDanger: "Xóa",
+        actionDo: EMPLOYEE_ACTION.DELETE
+      }
     }
   },
+
   created() {
     let me = this;
+    // Tiến hành load dữ liệu trong bảng
     this.loadTableData();
+
+    // Lắng nghe các sự kiện truyền qua eventBus
+    // Reload dữ liệu (nhấn btn refresh, auto reload sau post, put...)
     eventBus.$on("reloadTableData", function(toast) {
       me.loadTableData(toast);
     })
-    eventBus.$on("deleteTableData", function() {
-      me.deleteEntities();
-    })
+    // Lắng nghe sự kiện nhấn btn Xóa 
+    eventBus.$on("clickBtnDelete", this.clickBtnDelete );
+    // Lắng nghe sự kiện xóa nhân viên
+    eventBus.$on(EMPLOYEE_ACTION.DELETE, this.deleteEntities);
   },
+
   destroyed() {
     let me = this;
+    // Gỡ bỏ lắng nghe sự kiện
     eventBus.$off("reloadTableData", function(toast) {
       me.loadTableData(toast);
-    })
+    });
     eventBus.$off("deleteTableData", function() {
       me.deleteEntities();
-    })
+    });
+    eventBus.$off("clickBtnDelete", me.clickBtnDelete );
+    eventBus.$off(EMPLOYEE_ACTION.DELETE, this.deleteEntities);
   },
+
   methods: {
+    /**
+     * Lấy dữ liệu từ api và lưu vào tableData
+     * @param {Boolean} toast chỉ định việc hiện toast message khi load thành công
+     * @author pthieu (21-07-2021)
+     */
     loadTableData: function (toast = false) {
       // Gọi api thực hiện lấy dữ liệu
       axios.get(this.tableDataApi)
@@ -103,44 +128,85 @@ export default {
       });
       
     },
-     deleteEntities: function() {
-      if(this.selectedRows.length == 0) {
-        this.$toast(TOAST_TYPE.DANGER, "Chưa chọn bản ghi!");
-      }
 
-      let me = this;
+    /**
+     * Xử lý custom event "clickBtnDelete" trên eventBus (phát sinh khi nhấn btn Xóa)
+     * @author pthieu (21-07-2021)
+     */
+    clickBtnDelete: function() {
+      var len = this.selectedRows.length
+      if( len == 0) {
+        // TH chưa chọn bản ghi nào => thông báo "Chưa chọn..."
+        this.$toast(TOAST_TYPE.DANGER, "Chưa chọn bản ghi!");
+      } else {
+        // TH đã chọn bản ghi => hiện pop up yêu cầu xác nhận
+        this.dataPopUpDelete.content = `Bạn có chắc muốn "Xóa ${len} bản ghi" hay không?`;
+        eventBus.$emit("openPopUp", this.dataPopUpDelete);
+      }
+    },
+    /**
+     * Lấy dữ liệu từ api và lưu vào tableData
+     * @param {Boolean} toast chỉ định việc hiện toast message khi load thành công
+     * @author pthieu (21-07-2021)
+     */
+    deleteEntities: function() {
+      var len = this.selectedRows.length;
+
       Promise.all(
+        // Xóa lần lượt tất cả đối tượng được chọn
         this.selectedRows.map((id) => {
-          return axios.delete(`${this.deleteApi}/${id}`).then(res => console.log(res));
+          return axios.delete(`${this.deleteApi}/${id}`);
         })
       )
       .then(() => {
-        me.loadTableData();
-        me.selectedRows = [];
-      })
-      .then(() => {
-        console.log("Xóa")
-        this.$toast(TOAST_TYPE.SUCCESS, "Xóa thành công!");
+        // Thành công
+        this.selectedRows = [];
+        this.$toast(TOAST_TYPE.SUCCESS, `Xóa thành công ${len} bản ghi!`);
       })
       .catch(() => {
+        // Có lỗi
         this.$toast(TOAST_TYPE.DANGER, "Có lỗi xảy ra trong quá trình xóa!");
+      }).finally(() => {
+        // Luôn load lại dữ liệu
+        this.loadTableData();
       });    
     },
+    /**
+    * Xử lý sự kiện click chọn row
+    * @param {String} id trường id tương ứng của dữ liệu trong hàng
+    * @author pthieu (16-07-2021)
+    */
     clickRow: function(id) {
-      // console.log($event.target);
+      // Tìm xem id đã có trong danh sách được chọn chưa
       var index = this.selectedRows.indexOf(id);
       if(index == -1) {
+        // Nếu chưa thì thêm vào danh sách được chọn
         this.selectedRows.push(id);
       } else {
+        // Nếu đã có thì loại bỏ khỏi danh sách được chọn
         this.selectedRows.splice(index, 1);
       }
     },
+    /**
+    * Xử lý sự kiện dblclick chọn row => mở form thông tin chi tiết
+    * @param {String} id trường id tương ứng của dữ liệu trong hàng
+    * @author pthieu (16-07-2021)
+    */
     openDetailForm: function (id) {
       this.selectedRows = [id];
+      
+      // Truyền đi sự kiện (lên component cha) với tham số id
       this.$emit('openDetailForm', id);
     }
   },
   filters: {
+    /**
+     * Định dạng hiển thị dữ liệu
+     * @param {*} value giá trị dữ liệu cần định dạng
+     * @param {string} filterType kiểu định dạng
+     * @returns {*} dữ liệu sau định dạng
+     * @author pthieu (14-07-2021)
+     */
     formatData: function (value, filterType) {
       switch (filterType) {
         case "dateDDMMYYYY":
