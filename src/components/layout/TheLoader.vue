00b39741fe7bfa08525dbd26a4fb160855bbb14f
HieuPham2000 <hieu.pt183535@gmail.com>
2021-08-14 11:28:03 +0700

    add: loading text

--- src/components/layout/TheLoader.vue
+++ src/components/layout/TheLoader.vue
@@ -2,9 +2,22 @@
   <transition name="fade">
     <div class="loader-container">
       <div class="loader"></div>
+      <div class="loader-text">{{ text }}</div>
     </div>
   </transition>
 </template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: "Đang tải dữ liệu"
+    }
+  }
+}
+</script>
 <style scoped>
 .fade-enter-active, .face-leave-active {
   transition: opacity 0.5s;
@@ -25,9 +38,8 @@
 }
 .loader {
   position: absolute;
-  top: calc(50% - 20px);
-  left: calc(50% - 20px);
-  /* transform: translate(-50%, -50%); */
+  top: calc(50% - 24px);
+  left: calc(50% - 24px);
   border: 4px solid rgba(0, 0, 0, 0.1);
   border-top: 4px solid #ffffff;
   border-radius: 50%;
@@ -42,6 +54,34 @@
   }
   100% {
     transform: rotate(360deg);
+  }
+}
+
+.loader-text {
+  width: 100%;
+  position: absolute;
+  top: calc(50% + 30px) ;
+  text-align: center;
+}
+.loader-text::after {
+  font-size: 13px;
+  letter-spacing: 1px;
+  content: "";
+  animation: load-text 1.2s linear infinite;
+}
+
+@keyframes load-text {
+  0% {
+    content: "\00a0\00a0\00a0";
+  }
+  33% {
+    content: ".\00a0\00a0";
+  }
+  67% {
+    content: "..\00a0"
+  }
+  100% {
+    content: "..."
   }
 }
 </style>
